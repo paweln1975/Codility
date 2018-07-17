98ef37f82c08dd57f6c233da31043285e0e2baf1
pawel1975 <pawel.niedziela@gmail.com>
2018-07-17 11:25:16 +0200

    variable change

--- src/pl/paweln/codility/timecomplexity/FrogJump.java
+++ src/pl/paweln/codility/timecomplexity/FrogJump.java
@@ -2,7 +2,7 @@ package pl.paweln.codility.timecomplexity;
 
 public class FrogJump {
     public int solution (int X, int Y, int D) {
-        if (X > Y) throw new IllegalArgumentException("x=" + X + " must be lower or equal to y=" + Y);
+        if (X > Y) throw new IllegalArgumentException("X=" + X + " must be lower or equal to y=" + Y);
         if (X == Y) {
             return 0;
         } else {
