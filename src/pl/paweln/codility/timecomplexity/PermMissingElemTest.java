a8f699123a9336bd21ffc8491e20d828551b4a25
pawel1975 <pawel.niedziela@gmail.com>
2018-07-17 23:26:08 +0200

    fix test perm

--- src/pl/paweln/codility/timecomplexity/PermMissingElemTest.java
+++ src/pl/paweln/codility/timecomplexity/PermMissingElemTest.java
@@ -9,6 +9,8 @@ public class PermMissingElemTest {
         PermMissingElem t = new PermMissingElem();
         int [] tab = {};
         Assert.assertEquals(1, t.solution(tab));
+        Assert.assertEquals(1, t.solution2(tab));
+        Assert.assertEquals(1, t.solution3(tab));
 
     }
 
@@ -17,9 +19,13 @@ public class PermMissingElemTest {
         PermMissingElem t = new PermMissingElem();
         int [] tab = {1};
         Assert.assertEquals(2, t.solution(tab));
+        Assert.assertEquals(2, t.solution2(tab));
+        Assert.assertEquals(2, t.solution3(tab));
 
         tab = new int[] {2};
         Assert.assertEquals(1, t.solution(tab));
+        Assert.assertEquals(1, t.solution2(tab));
+        Assert.assertEquals(1, t.solution3(tab));
     }
 
     @Test
@@ -27,6 +33,8 @@ public class PermMissingElemTest {
         PermMissingElem t = new PermMissingElem();
         int [] tab = {1, 5, 7, 8, 9, 2, 3, 6};
         Assert.assertEquals(4, t.solution(tab));
+        Assert.assertEquals(4, t.solution2(tab));
+        Assert.assertEquals(4, t.solution3(tab));
     }
 
     @Test
@@ -34,6 +42,8 @@ public class PermMissingElemTest {
         PermMissingElem t = new PermMissingElem();
         int [] tab = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         Assert.assertEquals(1, t.solution(tab));
+        Assert.assertEquals(1, t.solution2(tab));
+        Assert.assertEquals(1, t.solution3(tab));
     }
 
     @Test
@@ -41,6 +51,8 @@ public class PermMissingElemTest {
         PermMissingElem t = new PermMissingElem();
         int [] tab = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         Assert.assertEquals(16, t.solution(tab));
+        Assert.assertEquals(16, t.solution2(tab));
+        Assert.assertEquals(16, t.solution3(tab));
     }
 
     @Test
@@ -53,12 +65,13 @@ public class PermMissingElemTest {
             tab[i] = i+1;
         }
         Assert.assertEquals(N + 1, t.solution(tab));
+        Assert.assertEquals(N + 1, t.solution2(tab));
+        Assert.assertEquals(N + 1, t.solution3(tab));
 
         // replace one element
         tab[5000] = N + 1;
         Assert.assertEquals(5001, t.solution(tab));
+        Assert.assertEquals(5001, t.solution2(tab));
+        Assert.assertEquals(5001, t.solution3(tab));
     }
-
-
-
 }
