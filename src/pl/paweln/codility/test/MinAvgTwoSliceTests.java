0b27105fbc7fa3c4e5803b8badc9ecc7e72f141b
pawel1975 <pawel.niedziela@gmail.com>
2018-10-09 23:52:04 +0200

    min avg two slice countDiv started

--- src/pl/paweln/codility/test/MinAvgTwoSliceTests.java
+++ src/pl/paweln/codility/test/MinAvgTwoSliceTests.java
@@ -26,4 +26,37 @@ public class MinAvgTwoSliceTests {
 
         Assert.assertEquals(expectedResult, result);
     }
+
+    @Test
+    public void test2Elements() {
+        int[] A = new int[] {5, 6};
+        int result = this.codilitySolution.solution(A);
+
+        Assert.assertEquals(0, result);
+    }
+
+    @Test
+    public void test4Elements() {
+        int[] A = new int[] {4, 3, 2, 1};
+        int result = this.codilitySolution.solution(A);
+
+        Assert.assertEquals(2, result);
+    }
+
+
+    @Test
+    public void testPerformance() {
+        int size = 100000;
+        int[] A = new int[size];
+        for (int i = 0; i < size - 3; i++) {
+            A[i] = 10000;
+        }
+        A[size - 3] = -10000;
+        A[size - 2] = -10000;
+        A[size - 1] = -10000;
+
+        int result = this.codilitySolution.solution(A);
+
+        Assert.assertEquals(size - 3, result);
+    }
 }
