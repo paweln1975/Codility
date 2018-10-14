0e284799b71064209d70634725a21ab7e0e0fa39
pawel1975 <pawel.niedziela@gmail.com>
2018-10-14 12:37:35 +0200

    distinct finished

---
+++ src/pl/paweln/codility/sorting/Distinct.java
@@ -0,0 +1,63 @@
+package pl.paweln.codility.sorting;
+
+import pl.paweln.codility.core.CodilitySolution;
+
+import java.util.Arrays;
+/*
+Write a function
+
+class Solution { public int solution(int[] A); }
+
+that, given an array A consisting of N integers, returns the number of distinct values in array A.
+
+For example, given array A consisting of six elements such that:
+
+ A[0] = 2    A[1] = 1    A[2] = 1
+ A[3] = 2    A[4] = 3    A[5] = 1
+the function should return 3, because there are 3 distinct values appearing in array A, namely 1, 2 and 3.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [0..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+ */
+
+public class Distinct implements CodilitySolution {
+
+    @Override
+    public int solution(int[] A) {
+        int distinct = 0;
+        if (A.length > 0) {
+            Arrays.sort(A);
+            distinct=1;
+        }
+        for (int i = 0; i < A.length-1; i++) {
+            if (A[i] != A[i+1]) {
+                distinct++;
+            }
+        }
+        return distinct;
+    }
+
+    @Override
+    public int solution(int N) {
+        return 0;
+    }
+
+    @Override
+    public Object solution(int[] A, int N) {
+        return null;
+    }
+
+
+
+    @Override
+    public int solution(int X, int Y, int Z) {
+        return 0;
+    }
+
+    @Override
+    public int[] solution(String S, int[] P, int[] Q) {
+        return new int[0];
+    }
+}
