7fef627fe14f3ee1bcd7e215414062f12b9d342c
Paweł <pawel.niedziela@gmail.com>
2020-10-20 11:02:46 +0200

    flags fast solution

--- src/pl/paweln/codility/prime/Flags.java
+++ src/pl/paweln/codility/prime/Flags.java
@@ -8,10 +8,73 @@ public class Flags implements CodilitySolution {
     public int[] solution(SolutionInputParams params) {
         int[] A = params.getFirstArray();
 
+        if (A.length == 0) {
+            throw new IllegalArgumentException("Array cannot be empty.");
+        }
+
+        if (A.length > 400000) {
+            throw new IllegalArgumentException("Max array size is 400000. Array too big N = " + A.length);
+        }
         return new int[] {this.solution(A)};
     }
 
     private int solution(int[] A) {
-        return 0;
+        return this.bisectionMethod(A);
+    }
+
+    private int bisectionMethod (int[] A) {
+        int a = 1;
+        int b = (int) Math.sqrt(A.length) + 1;
+        boolean[] peaks = this.createPeaks(A);
+        int maxFlags = 0;
+
+        while (a <= b) {
+            int x = (a+b)/2;
+            if (checkXFlags(x, peaks)) {
+                a = x + 1;
+                maxFlags = Math.max(maxFlags, x);
+            } else {
+                b = x - 1;
+            }
+        }
+
+        return maxFlags;
+    }
+
+    private boolean[] createPeaks (int[] A) {
+        boolean[] peaks = new boolean[A.length];
+        peaks[0] = false;
+        for (int i = 1; i < A.length - 1; i++) {
+            // check peak condition - neighbors are lower
+            if (A[i-1] < A[i] && A[i+1] < A[i]) {
+                peaks[i] = true;
+            }
+        }
+        peaks[A.length-1] = false;
+        return peaks;
+    }
+
+    /**
+     * the method checks if x flags could be set on given peaks
+     * @param x possible x flags
+     * @param peaks boolean table, true meas we have a peak
+     * @return true if x flags could be set on the peaks, otherwise false
+     */
+    private boolean checkXFlags (int x, boolean[] peaks) {
+        if (x == 0) {
+            throw new IllegalArgumentException("You cannot check 0 flags.");
+        }
+        int flags = x;
+        int pos = 0;
+        int N = peaks.length;
+        while (pos < N && flags > 0) {
+            if (peaks[pos]) {
+                flags--;
+                pos += x;
+            } else {
+                pos++;
+            }
+        }
+        return (flags == 0);
     }
 }
--- test/pl/paweln/codility/prime/FlagsTest.java
+++ test/pl/paweln/codility/prime/FlagsTest.java
@@ -27,4 +27,82 @@ public class FlagsTest {
 
         Assert.assertEquals(3, maxNumberFlags);
     }
+
+    @Test
+    public void testPackedPeaks() {
+        int[] A = {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0};
+        this.params = this.paramsBuilder.setFirstArray(A).build();
+        int maxNumberFlags = this.s.solution(this.params)[0];
+
+        Assert.assertEquals(4, maxNumberFlags);
+    }
+
+    @Test
+    public void testSingle() {
+        int[] A = {0};
+        this.params = this.paramsBuilder.setFirstArray(A).build();
+        int maxNumberFlags = this.s.solution(this.params)[0];
+
+        Assert.assertEquals(0, maxNumberFlags);
+    }
+
+    @Test
+    public void testDouble() {
+        int[] A = {0, 1};
+        this.params = this.paramsBuilder.setFirstArray(A).build();
+        int maxNumberFlags = this.s.solution(this.params)[0];
+
+        Assert.assertEquals(0, maxNumberFlags);
+    }
+
+    @Test
+    public void testTripple() {
+        int[] A = {0, 10, 4};
+        this.params = this.paramsBuilder.setFirstArray(A).build();
+        int maxNumberFlags = this.s.solution(this.params)[0];
+
+        Assert.assertEquals(1, maxNumberFlags);
+    }
+
+    @Test
+    public void testWithoutPeaks() {
+        int[] A = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        this.params = this.paramsBuilder.setFirstArray(A).build();
+        int maxNumberFlags = this.s.solution(this.params)[0];
+
+        Assert.assertEquals(0, maxNumberFlags);
+    }
+
+    @Test
+    public void testMultiplePeaks() {
+        int[] A = new int[200];
+        for (int i = 0; i < A.length; i++) {
+            if (i % 3 == 0) {
+                A[i] = 10;
+            } else {
+                A[i] = 5;
+            }
+        }
+        this.params = this.paramsBuilder.setFirstArray(A).build();
+        int maxNumberFlags = this.s.solution(this.params)[0];
+
+        Assert.assertEquals(14, maxNumberFlags);
+    }
+
+    @Test
+    public void testPerformance() {
+        int[] A = new int[400000];
+        for (int i = 0; i < A.length; i++) {
+            if (i % 2 == 0) {
+                A[i] = 1;
+            } else {
+                A[i] = 0;
+            }
+        }
+        this.params = this.paramsBuilder.setFirstArray(A).build();
+        int maxNumberFlags = this.s.solution(this.params)[0];
+
+        Assert.assertEquals(632, maxNumberFlags);
+    }
+
 }
